a766134c068db0f2765d87dae65deac75da7f641
Sergey Bartunov <sbos.net@gmail.com>
2015-05-12 17:34:43 +0300

    improved numerical stability

--- src/stick_breaking.jl
+++ src/stick_breaking.jl
@@ -10,13 +10,13 @@ function expected_logpi!{Tw <: Integer}(pi::Vector{Float64}, vm::VectorModel, w:
 	pi[:] = vm.counts[:, w] #view(vm.counts, :, w)
 	ts = sum(pi)
 	for k in 1:T(vm)-1
-		ts -= pi[k]
+		ts = max(ts - pi[k], 0.)
 		a, b = 1. + pi[k] - vm.d, vm.alpha + k*vm.d + ts
 		pi[k] = meanlog_beta(a, b) + r
 		r += meanlog_mirror(a, b)
 
 		pi_k = mean_beta(a, b) * x
-		x -= pi_k
+		x = max(x - pi_k, 0.)
 		if pi_k >= min_prob
 			senses += 1
 		end
@@ -32,11 +32,11 @@ function expected_pi!{Tw <: Integer}(pi::Vector{Float64}, vm::VectorModel,
 	senses = 0
 	ts = sum(view(vm.counts, :, w))
 	for k in 1:T(vm)-1
-		ts -= vm.counts[k, w]
+		ts = max(ts - vm.counts[k, w], 0.)
 		a, b = 1. + vm.counts[k, w] - vm.d, vm.alpha + k*vm.d + ts
 		pi[k] = mean_beta(a, b) * r
 		if pi[k] >= min_prob senses += 1 end
-		r -= pi[k]
+		r = max(r - pi[k], 0.)
 	end
 	pi[T(vm)] = r
 	if r >= min_prob senses += 1 end
