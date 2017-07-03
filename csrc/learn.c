4b486953e844bda8b176d62fcaf17d8ebabad4c1
glicerico <suarezandres@gmail.com>
2017-07-03 18:13:24 +0800

    Corrected bug printing clustering every training epoch

--- csrc/learn.c
+++ csrc/learn.c
@@ -158,10 +158,11 @@ void kmeans(char** words, float* syn0, int classes, int vocab_size,
         }
       }
       cl[c] = closeid;
-      // Save the K-means classes
-      fprintf(fo, "%s %d\n", words[c], cl[c]);
     }
   }
+
+  // Save the K-means classes
+  for (a = 0; a < vocab_size; a++) fprintf(fo, "%s %d\n", words[a], cl[a]);
 
   free(centcn);
   free(cent);
